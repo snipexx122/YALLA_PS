13bd4b8a953e75eb41edef1609f38fa4c23b586e
Mohamed864 <beherym884@gmail.com>
2024-05-22 22:02:19 +0200

    22/5/2024 11:00

--- yallaps/src/components/home.js
+++ yallaps/src/components/home.js
@@ -8,6 +8,10 @@ const Home = () => {
             <h1>Yalla Ps</h1>
             <div className="container">
                 <img src="https://arabgamerz.com/wp-content/uploads/2023/09/EA-FC-24-1200x628.webp"  />
+                <img src="https://gaming-cdn.com/images/products/9456/orig/league-of-legends-pc-game-cover.jpg?v=1662363312" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBdQLtq6jgMJlg6EPC9mNVvdubuL4o8vGnQboAXTm_hg&s" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSTq8NRO8JtZezQznXU8pzqmuBJrET1aR0LqAK1Y-QaA&s" />
+                <img src="https://i.ytimg.com/vi/4uMqsyK3TtA/maxresdefault.jpg" />
             </div>
         </div>
     );
